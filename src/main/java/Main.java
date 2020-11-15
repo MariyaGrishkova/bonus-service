6c3e61c6b9e50b2e3118d263dbbc42dd136bdc03
Mariya Grishkova <galeyevamv@gmail.com>
2020-11-15 11:28:12 +0300

    Revert "fixed#1"
    This reverts commit 3189ca38218c8151ba8f40d7b7ecf7759aef95f0.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,10 +5,7 @@ public class Main {
     long amount = 1000_60;
     boolean registered = true;
 
-    long expected = 30;
-    long actual = service.calculate(amount, registered);
-    boolean passed = expected == actual;
-    System.out.println(passed);
+    service.calculate(amount, registered);
   }
 }
 
